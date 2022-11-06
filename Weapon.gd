82bd380fc819b44106f56886ed0959c3aefdd385
happeecat <emma.rees.greville@gmail.com>
2022-11-06 13:54:20 -0800

    Added sword skin and swing animation

---
+++ Weapon.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_pressed("attack"):
+		$SwordAnimator.play("Swing")
