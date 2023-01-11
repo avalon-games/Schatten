0b361546493ce4e8b6b26b14df98313f4fb93677
nekopudding <deany.y.ca@gmail.com>
2023-01-10 20:18:07 -0800

    changed to global translation

--- Weapon.gd
+++ Weapon.gd
@@ -13,7 +13,7 @@ var space_state
 func _ready():
 	lightsource = $"../../../OmniLight"
 	player = $"../../../Player"
-	swordPivot = get_parent()
+	swordPivot = $"./Position3D/Sword Pivot"
 	swordTip = $"./Position3D/Sword Pivot/SwordTip"
 	space_state = get_world().direct_space_state
 
@@ -31,18 +31,22 @@ func _process(_delta):
 # then from sword pivot to the ground, and sword tip to the ground
 # if on the ground, the intersection between the two dots hit an enemy,
 # deal damage if not already hit during the current swing
+# need global translations for tip, pivot
 func deal_damage():
 	# cast ray from pivot to ground
-	var pivot_to = swordPivot.translation + (swordPivot.translation - lightsource.translation).normalized() * 10
-	var pivot_ray = space_state.intersect_ray(swordPivot.position, pivot_to,[player])
+	var pivot_to = swordPivot.get_global_translation() + (swordPivot.get_global_translation() - lightsource.get_global_translation()).normalized() * 50
+	var pivot_ray = space_state.intersect_ray(swordPivot.get_global_translation(), pivot_to,[player])
 	assert(pivot_ray != null) #make sure it hit the ground
-	var pivot_shadow_pos = pivot_ray.position
+	#get position for where the ray hits the ground
+	var pivot_shadow_pos = pivot_ray.position 
+	print('pivot',pivot_shadow_pos)
 	
 	# cast ray from swordTip to ground
-	var tip_to = swordTip.translation + (swordTip.translation - lightsource.translation).normalized() * 10
-	var tip_ray = space_state.intersect_ray(swordTip.translation, tip_to,[player])
+	var tip_to = swordTip.get_global_translation() + (swordTip.get_global_translation() - lightsource.get_global_translation()).normalized() * 50
+	var tip_ray = space_state.intersect_ray(swordTip.get_global_translation(), tip_to,[player])
 	assert(tip_ray != null)
 	var tip_shadow_pos = tip_ray.position
+	print('tip',tip_shadow_pos)
 	
 	
 		
